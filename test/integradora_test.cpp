d50b8e684c9cef64698fa3596e189f7b40ba9db4
armandochavez17 <a01720866@tec.mx>
2025-05-19 19:59:48 -0600

    Update integradora_test.cpp

---
+++ test/integradora_test.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+#include "../include/integradora.hpp"
+
+TEST(KMP, Found) {
+    EXPECT_EQ(kmpSearch("abcxabcdabcdabcy", "abcdabcy"), 9);
+}
+
+TEST(KMP, NotFound) {
+    EXPECT_EQ(kmpSearch("abcabcabc", "xyz"), -1);
+}
+
+TEST(LPS, CorrectValues) {
+    std::vector<int> expected = {0, 0, 0, 1, 2, 0, 1};
+    EXPECT_EQ(computeLPS("ababcab"), expected);
+}
+
+TEST(Palindrome, Centered) {
+    auto res = longestPalindrome("abcba");
+    EXPECT_EQ(res, std::make_pair(1, 5));
+}
+
+TEST(LCS, Found) {
+    auto res = findLongestCommonSubstring("abcdef", "zabcf");
+    EXPECT_EQ(res, std::make_pair(1, 3));
+}
+
+TEST(File, EmptyFileReturnsZero) {
+    EXPECT_EQ(longestPalindrome(""), std::make_pair(0, 0));
+}
